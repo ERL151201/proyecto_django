882441735f82b269e0e227856c5155ad10ec56b2
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-07-14 10:08:53 -0500

    añadiendo urls y views

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("Estás en la pagina principal de Premios Platzi App")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estás viendo la pregunta número {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estás viendo los resultados de la pregunta número {question_id}")    
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estás votando a la pregunta numero {question_id}")  
