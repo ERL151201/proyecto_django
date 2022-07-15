a9019ce3438e344994a4c7a050aa9906e239ce7e
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-07-15 17:02:26 -0500

    Ajustando detalles en los tests para IndexView

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,4 +1,5 @@
 import datetime
+from urllib import response
 
 from django.test import TestCase
 from django.urls.base import reverse
@@ -59,3 +60,27 @@ class QuestionIndexViewTests(TestCase):
         question = create_question("Past question ", days=-10)
         response = self.client.get(reverse("polls:index"))
         self.assertQuerysetEqual(response.context["latest_question_list"], [question])
+
+    def test_future_question_and_past_question(self):
+         """
+        Even if both past and future question exist , only past questions are displayed
+         """
+         past_question = create_question(question_text="Past question", days=-30)
+         future_question = create_question(question_text="Future question", days=30)
+         response = self.client.get(reverse("polls:index"))
+         self.assertQuerysetEqual(
+            response.context["latest_question_list"],
+            [past_question]
+        )
+
+    def test_two_past_questions(self):
+        """
+        The questions index page may displayed multiple questions
+        """
+        past_question1 = create_question(question_text="Past question 1", days=-30)
+        past_question2 = create_question(question_text="Future question 2", days=-40)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(
+                response.context["latest_question_list"],
+                [past_question1, past_question2]
+            )
